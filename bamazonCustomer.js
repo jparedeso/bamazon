0f97a20f70498c08d9e9c7dbcd63907f64bb1305
jparedeso <jparedeso@gmail.com>
2018-03-20 15:48:27 -0400

    Added schema and added functions to update  database and display cost of purchase.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,6 +4,8 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 var mysql = require("mysql");
 var choices = [];
+var itemId = "";
+var itemQuantity= "";
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -18,8 +20,19 @@ connection.connect(function(err) {
         return;
     }
     console.log("connected as id " + connection.threadId);
+    itemChoices();
     start();
 });
+
+function itemChoices() {
+    connection.query(
+        "SELECT * FROM products", function(err, results) {
+            if (err) throw err;
+            for (var i = 0; i < results.length; i++) {
+                choices.push(results[i].item_id);
+            }
+        });
+}
 
 function start() {
     inquirer.prompt([
@@ -35,17 +48,53 @@ function start() {
             message: "What quantity?"
         }
     ]).then(function(answer) {
-        var itemId = answer.itemId;
-        var itemQuantity = answer.itemQuantity;
+        itemId = answer.itemId;
+        itemQuantity = answer.itemQuantity;
         getItems(itemId, itemQuantity);
     });
 }
 function getItems(item, quantity) {
     connection.query(
-        "SELECT stock_quantity FROM products WHERE item_id = " + item, function() {
-
+        "SELECT stock_quantity FROM products WHERE item_id = " + item, function(err, results) {
+            if (err) throw err;
+            if (quantity > results[0].stock_quantity) {
+                console.log("There is not enough stock.");
+                getItems(itemId, itemQuantity);
+            } else {
+                placeOrder();
+            }
         });
 }
+
+function placeOrder() {
+    connection.query(
+        "UPDATE products SET stock_quantity -" + itemQuantity + " WHERE item_id = " +itemId, function(err, results) {
+           if (err) throw err;
+           var price = parseFloat(parseFloat(itemQuantity) * parseFloat(results[0].price)).toFixed(2);
+           console.log("You are buying " + itemId + "x" + itemQuantity + " for $" + price + ".");
+           itemId = "";
+           itemQuantity = "";
+           inquirer.prompt(
+               {
+                   name: "pick",
+                   type: "list",
+                   message: "Would you like to purchase anything else?",
+                   choices: ["Yes", "No"]
+               }
+               ).then(function(answer) {
+                   var pick = answer.pick;
+                   switch (pick) {
+                       case "Yes":
+                           start();
+                           break;
+                       case "No":
+                           connection.end();
+                           break;
+                   }
+           });
+        });
+}
+
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
--- schema.sql
+++ schema.sql
@@ -5,10 +5,11 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products(
-    item_id INT NOT NULL,
+    item_id VARCHAR(150) NOT NULL,
     product_name VARCHAR(300),
-    department_name VARCHAR(255),
-    price DECIMAL(18,2),
+    department_name VARCHAR(300),
+    price DECIMAL(10,2),
     stock_quantity INT,
     PRIMARY KEY (item_id)
 );
+SELECT * FROM bamazon.products;
